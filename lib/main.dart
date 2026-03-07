e0514cbef4a069e0be3395ca86b8ba72e255745d
syndrayaka <22010200@st.phenikaa-uni.edu.vn>
2026-03-07 18:00:35 +0700

    Update the application font
    Cập nhật font cho ứng dụng

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "syndrayaka's Story App",
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      theme: ThemeData(
+        fontFamily: 'DINNextLTPro',
+      ),
+      home: const SplashScreen(),
     );
   }
 }
